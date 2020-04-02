35558a2e39a3fc1e30f0abbbdcbe42aed6dea27c
Chris Richardson <chris@chrisrichardson.net>
2020-04-02 14:23:48 -0700

    Fixed deploy-artifacts.sh so it deploys to the correct repository

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -1,6 +1,5 @@
 #! /bin/bash -e
 
-
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [  $BRANCH == "master" ] ; then
@@ -11,12 +10,24 @@ if [  $BRANCH == "master" ] ; then
 fi
 
 if ! [[  $BRANCH =~ ^[0-9]+ ]] ; then
-  echo Not release $BRANCH
+  echo Not release $BRANCH - no PUSH
   exit 0
+elif [[  $BRANCH =~ RELEASE$ ]] ; then
+  BINTRAY_REPO_TYPE=release
+elif [[  $BRANCH =~ M[0-9]+$ ]] ; then
+    BINTRAY_REPO_TYPE=milestone
+elif [[  $BRANCH =~ RC[0-9]+$ ]] ; then
+    BINTRAY_REPO_TYPE=rc
+else
+  echo cannot figure out bintray for this branch $BRANCH
+  exit -1
 fi
+
+echo BINTRAY_REPO_TYPE=${BINTRAY_REPO_TYPE}
 
 VERSION=$BRANCH
 
 $PREFIX ./gradlew -P version=${VERSION} \
-  -P deployUrl=https://dl.bintray.com/eventuateio-oss/eventuate-maven-release \
+  -P bintrayRepoType=${BINTRAY_REPO_TYPE} \
+  -P deployUrl=https://dl.bintray.com/eventuateio-oss/eventuate-maven-${BINTRAY_REPO_TYPE} \
   testClasses bintrayUpload
