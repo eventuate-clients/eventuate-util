14dbb45de3c8e7b86786c0ab57183ad44b8a23e1
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2021-05-28 11:03:31 +0300

    Improved swagger configuration

--- eventuate-util-spring-swagger/src/main/java/io/eventuate/util/spring/swagger/CommonSwaggerAutoConfiguration.java
+++ eventuate-util-spring-swagger/src/main/java/io/eventuate/util/spring/swagger/CommonSwaggerAutoConfiguration.java
@@ -1,30 +1,12 @@
 package io.eventuate.util.spring.swagger;
 
-import com.fasterxml.classmate.TypeResolver;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-@EnableSwagger2
 @ConditionalOnBean(EventuateSwaggerConfig.class)
+@Import(CommonSwaggerConfiguration.class)
 public class CommonSwaggerAutoConfiguration {
 
-  @Bean
-  public Docket api(EventuateSwaggerConfig eventuateSwaggerConfig,
-                    TypeResolver typeResolver) {
-
-    return new Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage(eventuateSwaggerConfig.requestHandlerSelectorsBasePackage()))
-            .build()
-            .pathMapping("/")
-            .genericModelSubstitutes(ResponseEntity.class)
-            .useDefaultResponseMessages(false);
-  }
 }
