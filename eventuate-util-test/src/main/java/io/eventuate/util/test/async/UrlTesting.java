a567bf17da10a893739eb83f49f0216ff28d9654
Chris Richardson <chris@chrisrichardson.net>
2022-07-22 15:28:36 -0700

    Fixed #18 UrlTesting.assertUrlStatusIsOk() needs to generate a useful message

--- eventuate-util-test/src/main/java/io/eventuate/util/test/async/UrlTesting.java
+++ eventuate-util-test/src/main/java/io/eventuate/util/test/async/UrlTesting.java
@@ -7,9 +7,17 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class UrlTesting {
-  public static void assertUrlStatusIsOk(String host, int port, String path) throws IOException {
-    HttpURLConnection connection = (HttpURLConnection)new URL(String.format("http://%s:%s%s", host, port, path)).openConnection();
+  public static void assertUrlStatusIsOk(String host, int port, String path) {
+    assertUrlStatusIsOk(String.format("http://%s:%s%s", host, port, path));
+  }
 
-    Assert.assertEquals(200, connection.getResponseCode());
+  public static void assertUrlStatusIsOk(String url) {
+    try {
+      HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+      if (connection.getResponseCode() != 200)
+        Assert.fail(String.format("Expected 200 for %s, got %s", url, connection.getResponseCode()));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
